72784d78dcf2d2a871d29596101110ef807d1876
warzsummer-blip <302414655+warzsummer-blip@users.noreply.github.com>
2026-08-03 16:06:16 +0900

    ユーザー認証機能完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
+    protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
+
+    def after_sign_in_path_for(resource)
+        posts_path
+    end
+    
 end
