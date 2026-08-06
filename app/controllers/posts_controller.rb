f39b0ba4178e188293903b2c3d96084c343a6dcf
warzsummer-blip <302414655+warzsummer-blip@users.noreply.github.com>
2026-08-06 13:35:32 +0900

    検索機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.page(params[:page]).reverse_order
+    @posts = @posts.where('location LIKE ?', "%#{params[:search]}%") if params[:search].present?
   end
 
   def show
